24d1d49b65612b6d3027f5dcffc7f7f5446f63fe
isaaclevi <isaaclevi1710@gmail.com>
2015-01-14 18:42:52 +0200

    מחיקת הבדיקות
    מחקתי את הבדיקות

--- app/src/main/java/com/isaaclevi/event_app/danieltest.java
+++
@@ -1,7 +0,0 @@
-package com.isaaclevi.event_app;
-
-/**
- * Created by schmidtda on 14/01/2015.
- */
-public class danieltest {
-}
--- app/src/main/java/com/isaaclevi/event_app/test.java
+++
@@ -1,7 +0,0 @@
-package com.isaaclevi.event_app;
-
-/**
- * Created by isaac on 14/01/2015.
- */
-public class test {
-}
