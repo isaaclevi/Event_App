e4395cfcada03837faefcdc9e6bc90b247685ce3
isaaclevi <isaaclevi1710@gmail.com>
2015-02-11 21:28:22 +0200

    last changes

--- app/src/main/java/com/isaaclevi/event_app/AddEventFragment.java
+++ app/src/main/java/com/isaaclevi/event_app/AddEventFragment.java
@@ -6,8 +6,11 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.Calendar;
 
 
 /**
@@ -22,6 +25,15 @@ public class AddEventFragment extends Fragment {
     String PersonName;
     String NickName;
 
+    TextView date;
+    DatePicker datePicker;
+
+    private int day;
+    private int month;
+    private int year;
+
+    static int date_dialog_id;
+
     public AddEventFragment()
     {
         // Required empty public constructor
@@ -31,6 +43,28 @@ public class AddEventFragment extends Fragment {
     {
         this.PersonName = UserName;
         this.NickName = NickName;
+    }
+
+    public void setCurrentDateOnView(View root)
+    {
+        /*
+        date = (TextView) root.findViewById(R.id.);
+        datePicker = (DatePicker) root.findViewById(R.id.);
+
+        final Calendar c = Calendar.getInstance();
+        year = c.get(Calendar.YEAR);
+        month = c.get(Calendar.MONTH);
+        day = c.get(Calendar.DAY_OF_MONTH);
+
+        // set current date into textview
+        tvDisplayDate.setText(new StringBuilder()
+                // Month is 0 based, just add 1
+                .append(month + 1).append("-").append(day).append("-")
+                .append(year).append(" "));
+
+        // set current date into datepicker
+        dpResult.init(year, month, day, null);
+        */
     }
 
     @Override
@@ -43,6 +77,10 @@ public class AddEventFragment extends Fragment {
         EventName = (EditText) root.findViewById(R.id.add_event_name);
         EventExplanation = (EditText) root.findViewById(R.id.add_event_explanation);
         EventAddress = (EditText) root.findViewById(R.id.add_event_address);
+
+        setCurrentDateOnView(root);
+        //addListenerOnButton();
+
         return root;
     }
 
